b6de78b135d5ea0091fe16c6e9b32548861b832a
Ankur Gupta <68587598+ankur-gupta-29@users.noreply.github.com>
2025-04-06 13:32:05 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_ankur-gupta-29-4-bit-barrel-shifter user_project (
+  tt_um_ankur-gupta-29-barrel-shifter user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
