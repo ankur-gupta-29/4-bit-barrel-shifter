3ffc2867aa4f0a864ea159a63ec181716be732bd
Ankur Gupta <68587598+ankur-gupta-29@users.noreply.github.com>
2025-04-06 13:35:02 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_ankur-gupta-29-barrel-shifter (
+module tt_um_ankur_gupta_29_4bit_barrel_shifter (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
